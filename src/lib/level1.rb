f97019c8e4fa52514ba35b07a0e7f171a71f9a43
iagox86 <ron-git@skullsecurity.org>
2019-01-06 10:28:27 -0800

    Implement level 1

--- src/lib/level1.rb
+++ src/lib/level1.rb
@@ -1,9 +1,41 @@
+# Base32 / Base64 / Hex / binary / urlencode / ascii (decimal) etc
+require 'base64'
+require 'base32'
+
+def base64(text)
+  return Base64.encode64(text)
+end
+
+def hex(text)
+  return text.unpack("H*")[0].gsub(/(..)/, '\1 ')
+end
+
+def binary(text)
+  return text.unpack('B*')[0].gsub(/(........)/, '\1 ')
+end
+
+def urlenc(text)
+  return text
+end
+
+def ascii(text)
+  return text.bytes().join(" ")
+end
+
+def base32(text)
+  return Base32.encode(text).downcase
+end
+
+def everything(text)
+  return hex(ascii(urlenc(base32(base64(text)))))
+end
+
 module Cryptorama
   class Server < Sinatra::Base
     LEVEL1 = {
-      name:   "Level 1: Encoding and decoding",
+      name:   "Level 1: The package thief!",
       url:    "/level1",
-      answer: 'level2unlocked',
+      answer: 'Dr.Z',
     }
 
     get '/level1' do
@@ -13,7 +45,7 @@ module Cryptorama
     end
 
     post '/level1' do
-      if params[:answer] == LEVEL1[:answer]
+      if params[:answer].downcase == LEVEL1[:answer].downcase
         session[:level2][:open] = true
         session[:level1][:completed] = true
 
